1c07b850016ffa04658a25f71eca3a2ab4a59ac8
Venkata Damarla <venkatdamarla@gmail.com>
2024-12-30 18:12:31 +0530

    Variable file added by Venkat

---
+++ 02.Variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Conversation"
+echo "Dad:  Hi Gayatri, What you are doing"
+echo "Gayatri: I am doing Maths"
+echo "Dad:  How many marks you will get it"
+echo "Gayatri:  80% marks will get it"
