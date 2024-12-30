e51cbbd7a477481207b7814c617ecf86ea684ceb
Venkata Damarla <venkatdamarla@gmail.com>
2024-12-30 19:20:47 +0530

    Variables concept used

--- 02.Variables.sh
+++ 02.Variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+Father=Venkat
+Daughter=Gayatri
 echo "Conversation"
-echo "Dad:  Hi Gayatri, What you are doing"
-echo "Gayatri: I am doing Maths"
-echo "Dad:  How many marks you will get it"
-echo "Gayatri:  80% marks will get it"
+echo "$Father:  Hi $Daughter, What you are doing"
+echo "$Daughter: I am doing Maths"
+echo "$Father:  How many marks you will get it"
+echo "$Daughter:  80% marks will get it"
