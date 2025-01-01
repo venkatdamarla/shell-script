05835af3b01131748cf879b7eff28701790b2d57
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-01 13:42:37 +0530

    Array program updated

--- 07.array.sh
+++ 07.array.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 #The above line represents shebang interpreter
 movies=("Annamayya" "Sriramadasu" "Srirama")
-echo "The first movie is : ${movies[0]}
+echo "The first movie is : ${movies[0]}"
