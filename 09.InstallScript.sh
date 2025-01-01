50de16d66db6bc6dcddb13b181b3281642df6dba
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-01 17:04:59 +0530

    Instllation script updated

--- 09.InstallScript.sh
+++ 09.InstallScript.sh
@@ -5,6 +5,14 @@ if [ $USERID -eq 0 ]
 then
    echo "The user has the sudo access and he can install the softwares"
 else
-   echo "The user doesn't have the root access and he can't install the softwares"
+   echo "The user doesn't have the sudo access and he can't install the softwares"
    exit 1
+fi
+dnf install mysqll -y
+if [ $? -ne 0 ]
+then
+  echo "My sql installation ..... FAILURE"
+  exit 1
+else
+   echo "My sql installation ..... SUCCESS"
 fi
