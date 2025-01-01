cb5edfeb7bd5c8013eed6eada8bc6a24c5ef7bb3
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-01 16:54:11 +0530

    Installation Script

--- 09.InstallScript.sh
+++ 09.InstallScript.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)
 echo "The UserID value is: $USERID"
+if [ $USERID -eq 0 ] then
+   echo " The user has the root access and he can install the softwares"
+else
+   echo " The user doesn't have the root access and he can't install the softwares"
+   exit 1
+fi
