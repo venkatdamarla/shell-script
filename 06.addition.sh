83cb5a42a32c9adedc580c6d356f32c8bb2d222c
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-01 07:53:15 +0530

    date part added

--- 06.addition.sh
+++ 06.addition.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 number1=$1
 number2=$2
-sum=$($number1+$number2)
+sum=$(($number1+$number2))
 echo "The sum of $number1 and $number2 is : $sum"
+timestamp=$(date)
+echo " This program written at: $timestamp"
