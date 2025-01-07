f2899cdb7c64292a0fc464ebfa52495632ffdbb5
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-07 15:39:40 +0530

    Backup Script

--- backup.sh
+++ backup.sh
@@ -6,9 +6,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 Logs_Folder="/var/log/shellscript-logs"
+echo $Logs_Folder
 Log_File="$( echo $0 | cut -d "." -f1 )
+echo $Log_File
 Timestamp=$(year +"%Y-%m-%d-%H-%M-%S)
+echo $Timestamp
 Log_File_Name="$Logs_Folder/$Log_File-$Timestamp.log"
 echo "Script started executing at: $Timestamp" &>>$Log_File_Name
-echo "What is the problem"
-echo "Venkata"
