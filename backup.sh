55aa6f81d7c996d95d9d166f522faf8e30cb93f6
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-07 16:19:59 +0530

    Backup Script

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ Logs_Folder="/var/log/shellscript-logs"
 echo $Logs_Folder
 Log_File=$(echo $0 | cut -d "." -f 1)
 echo $Log_File
-Timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+Timestamp=$(date +%Y-%m-%d-%H-%M-%S)
 echo $Timestamp
 Log_File_Name="$Logs_Folder/$Log_File-$Timestamp.log"
 echo $Log_File_Name
