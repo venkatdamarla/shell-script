8181167859cd7ea6f3da227c8773dcee8dca5c91
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-07 16:59:30 +0530

    Backup with args

--- backup.sh
+++ backup.sh
@@ -5,17 +5,16 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+#Source_Directory="/home/ec2-user/source_logs"
+#Dest_Directory="/home/ec2-user/dest_logs"
 Source_Directory=$1
 Dest_Directory=$2
 Days=${3: -14}
 Logs_Folder="/var/log/shellscript-logs"
-echo $Logs_Folder
 Log_File=$(echo $0 | cut -d "." -f 1)
-echo $Log_File
 Timestamp=$(date +%Y-%m-%d-%H-%M-%S)
-echo $Timestamp
 Log_File_Name="$Logs_Folder/$Log_File-$Timestamp.log"
-echo $Log_File_Name
+
 Usage()
 {
     echo "Usage :: sh backup.sh <Source-Directory> <Dest-Directory> <No of Days(Optional>"
@@ -26,6 +25,14 @@ echo -e "$G The script execution started at :$Timestamp $N" &>>$Log_File_Name
 if [ $# -lt 2 ]
 then
     Usage
+fi
+if [ ! -d $Source_Directory ]
+then
+   echo " The Source Directory not exits"
+fi
+if [ ! -d $Dest_Directory ]
+then
+  echo "THe Destination directory not exists"
 fi
 
 
