3526b3cc6b209da61c5adb8ee55fabae397f95b0
Venkata Damarla <venkatdamarla@gmail.com>
2025-01-07 15:50:59 +0530

    Backup Script

--- backup.sh
+++ backup.sh
@@ -9,7 +9,4 @@ Logs_Folder="\var\log\shellscript-logs"
 echo $Logs_Folder
 Log_File=$(echo $0 | cut -d "." -f 1)
 echo $Log_File
-Timestamp=$(year +"%Y-%m-%d-%H-%M-%S)
-echo $Timestamp
-Log_File_Name="$Logs_Folder/$Log_File-$Timestamp.log"
-echo "Script started executing at: $Timestamp" &>>$Log_File_Name
+
